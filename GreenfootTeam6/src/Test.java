a2ebb1c344c1389f6f42af9f5ddd795d9ad7582b
x2spieler <ju_opitz@web.de>
2015-10-13 18:16:57 +0200

    commit for testing purposes.

---
+++ GreenfootTeam6/src/Test.java
@@ -0,0 +1,16 @@
+
+/**
+ * @author opitzju, oertelt
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
