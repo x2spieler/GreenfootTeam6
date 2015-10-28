04c7a19d58f23fd78edf276130512eb492fd15ec
Robin Drangmeister <robin.drangmeister@outlook.com>
2015-10-28 10:17:51 +0100

    Testting merge

--- DugeonGeneration/src/AI/TestForAIMain.java
+++ DugeonGeneration/src/AI/TestForAIMain.java
@@ -6,5 +6,13 @@ public class TestForAIMain
 	{
 		/*Enemy e=new Enemy();
 		e.findPath(new Point(2, 2), new Point(48, 48));*/
+		System.out.println(rec(20));
+	}
+	
+	private static double rec(int n)
+	{
+		if(n!=1)
+			return 2.d-1.d/rec(n-1);
+		return 2;
 	}
 }
