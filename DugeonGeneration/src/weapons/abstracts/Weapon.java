4a0b1e657fb666e573b4e1d7426ae5171ffc71d2
x2spieler <ju_opitz@web.de>
2015-12-08 18:21:08 +0100

    Added new animation for sword attack

--- DugeonGeneration/src/weapons/abstracts/Weapon.java
+++ DugeonGeneration/src/weapons/abstracts/Weapon.java
@@ -1,4 +1,5 @@
 package weapons.abstracts;
+
 import java.awt.geom.Point2D;
 
 import AI.Enemy;
@@ -7,111 +8,104 @@ import greenfoot.Greenfoot;
 import greenfoot.GreenfootImage;
 import greenfoot.MouseInfo;
 import greenfoot.World;
-import player.Player;
 import scrollWorld.ScrollActor;
-import sun.security.util.UntrustedCertificates;
 import weapons.EntityType;
 import world.DungeonMap;
 
-public abstract class Weapon extends ScrollActor
-{
+public abstract class Weapon extends ScrollActor {
 	protected int damage = -1;
 	protected int reloadTimeInMS = -1;
 	protected long lastUsage = -1;
 	protected int additionalValue = -1; // value that adds to the enemys value
-	protected ScrollActor owner=null;
+	protected ScrollActor owner = null;
 	protected GreenfootImage[] animImages;
 	protected String weaponName;
-	protected Point2D weaponOffsetToPlayer=null;
-	protected int ticksPerAnimImg=-1;
+	protected Point2D weaponOffsetToPlayer = null;
+	protected int ticksPerAnimImg = -1;
 	protected IWorldInterfaceForAI wi = null;
-	protected boolean playAnimation=false;
-	protected EntityType typeToIgnore=null;
-	GreenfootImage emptyImage=null;
+	protected boolean playAnimation = false;
+	protected EntityType typeToIgnore = null;
+	GreenfootImage emptyImage = null;
 	protected boolean isLongRangeWeapon;
 	protected boolean isPlayerWeapon;
-	protected boolean launchLongRangeWeapon=false;
-	protected String displayName="Unknown name";
-	
-	private int showWeaponWhileChanging=0;
-	private final int SHOW_WEAPON_ON_CHANGE_DUR=30;
-	private int prevRotation=0;
-	private int animTicks=0;
-	private boolean active=false;
+	protected boolean launchLongRangeWeapon = false;
+	protected String displayName = "Unknown name";
+	protected int anim_frame_count;
+	protected int default_anim_frame = 0;
+
+	private int showWeaponWhileChanging = 0;
+	private final int SHOW_WEAPON_ON_CHANGE_DUR = 30;
+	private int prevRotation = 0;
+	private int animTicks = 0;
+	private boolean active = false;
 	private final int MAX_WEAPON_ROTATION = 22;
 
-	public Weapon()
-	{
-		animImages=new GreenfootImage[4];
+	public Weapon() {
+		anim_frame_count = getAnimFrameCount();
+		animImages = new GreenfootImage[anim_frame_count];
 	}
-	
-	public void activateWeapon()
-	{
-		active=true;
-		setImage(animImages[0]);
-		showWeaponWhileChanging=SHOW_WEAPON_ON_CHANGE_DUR;
+
+	protected int getAnimFrameCount() {
+		return 4;
 	}
-	
-	public void deactivateWeapon()
-	{
-		playAnimation=false;
-		active=false;
+
+	public void activateWeapon() {
+		active = true;
+		setImage(animImages[default_anim_frame]);
+		showWeaponWhileChanging = SHOW_WEAPON_ON_CHANGE_DUR;
+	}
+
+	public void deactivateWeapon() {
+		playAnimation = false;
+		active = false;
 		setImage(emptyImage);
-		showWeaponWhileChanging=0;
+		showWeaponWhileChanging = 0;
 	}
-	
-	public String getDisplayName()
-	{
+
+	public String getDisplayName() {
 		return displayName;
 	}
-	
-	public String getWeaponName()
-	{
+
+	public String getWeaponName() {
 		return weaponName;
 	}
-	
-	public boolean isPlayingAnimation()
-	{
+
+	public boolean isPlayingAnimation() {
 		return playAnimation;
 	}
 
 	@Override
-	protected void addedToWorld(World world) 
-	{
+	protected void addedToWorld(World world) {
 		super.addedToWorld(world);
-		wi=(IWorldInterfaceForAI) world;
-		if(wi==null)
-		{
+		wi = (IWorldInterfaceForAI) world;
+		if (wi == null) {
 			System.out.println("Can't cast world to WorldInterfaceForAI\nSomething's clearly wrong!");
 		}
-		weaponOffsetToPlayer=new Point2D.Double(wi.getTileSize()/2,0);
-		typeToIgnore=(owner instanceof Enemy ? EntityType.ENEMY : EntityType.PLAYER);
-		isPlayerWeapon=typeToIgnore==EntityType.PLAYER;
+		if (weaponOffsetToPlayer == null)
+			weaponOffsetToPlayer = new Point2D.Double(wi.getTileSize() / 2, 0);
+		typeToIgnore = (owner instanceof Enemy ? EntityType.ENEMY : EntityType.PLAYER);
+		isPlayerWeapon = typeToIgnore == EntityType.PLAYER;
 		loadImages();
 	}
 
 	@Override
-	public void act()
-	{
-		if((isPlayerWeapon&&active)||playAnimation)
-		{
-			rotatePoint(weaponOffsetToPlayer, getRotation()-prevRotation);
-			setGlobalLocation(owner.getGlobalX()+(int)weaponOffsetToPlayer.getX(), owner.getGlobalY()+(int)weaponOffsetToPlayer.getY());
-			prevRotation=getRotation();
-			if(isPlayerWeapon)
-			{
+	public void act() {
+		if ((isPlayerWeapon && active) || playAnimation) {
+			rotatePoint(weaponOffsetToPlayer, getRotation() - prevRotation);
+			setGlobalLocation(owner.getGlobalX() + (int) weaponOffsetToPlayer.getX(),
+					owner.getGlobalY() + (int) weaponOffsetToPlayer.getY());
+			prevRotation = getRotation();
+			if (isPlayerWeapon) {
 				MouseInfo info = Greenfoot.getMouseInfo();
 				if (info != null) {
 					int x = info.getX();
 					int y = info.getY();
 					turnTowards(x, y);
 				}
-			}
-			else
-			{
+			} else {
 				setRotation(owner.getRotation());
 			}
-			int walkRot=owner.getRotation();
+			int walkRot = owner.getRotation();
 			int currRot = getRotation();
 			int minAngle = walkRot - MAX_WEAPON_ROTATION;
 			int maxAngle = walkRot + MAX_WEAPON_ROTATION;
@@ -126,29 +120,24 @@ public abstract class Weapon extends ScrollActor
 			else if (currRot > maxAngle)
 				setRotation(maxAngle);
 		}
-		if(playAnimation)
-		{
-			if(animTicks%ticksPerAnimImg==0)
-				setImage(animImages[animTicks/ticksPerAnimImg]);
+		if (playAnimation) {
+			if (animTicks % ticksPerAnimImg == 0)
+				setImage(animImages[animTicks / ticksPerAnimImg]);
 			animTicks++;
-			if(animTicks==4*ticksPerAnimImg)
-			{
-				animTicks=0;
-				playAnimation=false;
-				if(owner instanceof Enemy)
-				{
-					((Enemy)owner).stopAttacking();
+			if (animTicks == 4 * ticksPerAnimImg) {
+				animTicks = 0;
+				playAnimation = false;
+				if (owner instanceof Enemy) {
+					((Enemy) owner).stopAttacking();
 				}
 				setImage(emptyImage);
 
-				launchLongRangeWeapon=true;
+				launchLongRangeWeapon = true;
 			}
-		}
-		else
-		{
-			if(getImage()!=emptyImage&&showWeaponWhileChanging==0)
+		} else {
+			if (getImage() != emptyImage && showWeaponWhileChanging == 0)
 				setImage(emptyImage);
-			if(showWeaponWhileChanging>0)
+			if (showWeaponWhileChanging > 0)
 				showWeaponWhileChanging--;
 		}
 	}
@@ -156,28 +145,24 @@ public abstract class Weapon extends ScrollActor
 	/**
 	 * @return If the weapon was fired (in this term a sword is able to fire, doesn't matter). Also pays attention to the ammo
 	 */
-	public boolean use()
-	{
+	public boolean use() {
 		long millisNow = DungeonMap.getGreenfootTime();
-		if (lastUsage + reloadTimeInMS < millisNow)
-		{
-			if(triggerUse())
-			{
+		if (lastUsage + reloadTimeInMS < millisNow) {
+			if (triggerUse()) {
 				lastUsage = millisNow;
-				playAnimation=true;
-				animTicks=0;
-				showWeaponWhileChanging=0;
+				playAnimation = true;
+				animTicks = 0;
+				showWeaponWhileChanging = 0;
 				return true;
-			}	
+			}
 		}
 		return false;
 	}
-	
-	public void resetWeapon()
-	{
-		playAnimation=false;
-		animTicks=0;
-		launchLongRangeWeapon=false;
+
+	public void resetWeapon() {
+		playAnimation = false;
+		animTicks = 0;
+		launchLongRangeWeapon = false;
 	}
 
 	/**
@@ -185,74 +170,62 @@ public abstract class Weapon extends ScrollActor
 	 */
 	protected abstract boolean triggerUse();
 
-	public int getAdditionalValue()
-	{
+	public int getAdditionalValue() {
 		return additionalValue;
 	}
 
-	protected void loadImages()
-	{
-		for(int i=0;i<4;i++)
-		{
-			animImages[i]=new GreenfootImage("enemies/weapons/"+weaponName+"/"+weaponName+i+".png");
+	protected void loadImages() {
+		for (int i = 0; i < anim_frame_count; i++) {
+			animImages[i] = new GreenfootImage("enemies/weapons/" + weaponName + "/" + weaponName + i + ".png");
 		}
-		emptyImage=new GreenfootImage("empty.png");
-		if(isPlayerWeapon)
+		emptyImage = new GreenfootImage("empty.png");
+		if (isPlayerWeapon)
 			setImage(animImages[0]);
 		else
 			setImage(emptyImage);
 	}
 
 	//Implementation credits: http://stackoverflow.com/questions/2259476/rotating-a-point-about-another-point-2d
-	protected void rotatePoint(Point2D point, int angle)
-	{
-		double s=Math.sin(Math.toRadians(angle));
-		double c=Math.cos(Math.toRadians(angle));
+	protected void rotatePoint(Point2D point, int angle) {
+		double s = Math.sin(Math.toRadians(angle));
+		double c = Math.cos(Math.toRadians(angle));
 
-		double xNew=point.getX()*c-point.getY()*s;
-		double yNew=point.getX()*s+point.getY()*c;
+		double xNew = point.getX() * c - point.getY() * s;
+		double yNew = point.getX() * s + point.getY() * c;
 		point.setLocation(xNew, yNew);
 	}
 
-	
 	//GETTER / SETTER
-	
-	public int getDamage()
-	{
+
+	public int getDamage() {
 		return damage;
 	}
-	
-	public void setDamage(int dmg)
-	{
-		damage=dmg;
+
+	public void setDamage(int dmg) {
+		damage = dmg;
 	}
-	
-	public int getReloadTimeInMs()
-	{
+
+	public int getReloadTimeInMs() {
 		return reloadTimeInMS;
 	}
-	
-	public void setReloadtimeInMs(int ms)
-	{
-		reloadTimeInMS=ms;
+
+	public void setReloadtimeInMs(int ms) {
+		reloadTimeInMS = ms;
 	}
-	
-	public void setAdditionalValue(int val)
-	{
-		additionalValue=val;
+
+	public void setAdditionalValue(int val) {
+		additionalValue = val;
 	}
 
 	public boolean isLongRangeWeapon() {
 		return isLongRangeWeapon;
 	}
-	
-	public void setTicksPerAnimImg(int ticks)
-	{
-		ticksPerAnimImg=ticks;
+
+	public void setTicksPerAnimImg(int ticks) {
+		ticksPerAnimImg = ticks;
 	}
-	
-	public int geTicksPerAnimImg()
-	{
+
+	public int geTicksPerAnimImg() {
 		return ticksPerAnimImg;
 	}
 }
--- DugeonGeneration/src/weapons/short_range/Sword.java
+++ DugeonGeneration/src/weapons/short_range/Sword.java
@@ -1,18 +1,27 @@
 package weapons.short_range;
 
+import java.awt.geom.Point2D;
+
 import scrollWorld.ScrollActor;
 import weapons.abstracts.ShortRangeWeapon;
+import world.DungeonMap;
 
-public class Sword extends ShortRangeWeapon
-{
-	public Sword(ScrollActor owner)
-	{
-		damage=2000;
-		weaponName="sword";
-		displayName="Sword";
-		this.owner=owner;
-		reloadTimeInMS=750;
-		additionalValue=1;
-		ticksPerAnimImg=5;
+public class Sword extends ShortRangeWeapon {
+	public Sword(ScrollActor owner) {
+		damage = 2000;
+		weaponName = "sword";
+		displayName = "Sword";
+		this.owner = owner;
+		reloadTimeInMS = 750;
+		additionalValue = 1;
+		ticksPerAnimImg = 4;
+		anim_frame_count = 6;
+		weaponOffsetToPlayer = new Point2D.Double(DungeonMap.TILE_SIZE, 0);
+		default_anim_frame = 5;
+	}
+
+	@Override
+	protected int getAnimFrameCount() {
+		return 6;
 	}
 }
